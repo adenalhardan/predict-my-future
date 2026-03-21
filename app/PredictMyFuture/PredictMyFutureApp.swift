af68d22272713932cf93ee3d17a8791bcea008c2
Aden  Al-Hardan <adenalhardan@gmail.com>
2026-03-21 10:35:54 -0700

    App

---
+++ app/PredictMyFuture/PredictMyFutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictMyFutureApp.swift
+//  PredictMyFuture
+//
+//  Created by Aden  Al-Hardan on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct PredictMyFutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
